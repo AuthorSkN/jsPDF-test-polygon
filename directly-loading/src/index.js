7fd7cb73ca48cacfb06e4cda585efa827f4d0387
nisk0817 <nikolai.skladnev@netcracker.com>
2020-07-31 08:13:59 +0400

    add PTSans font directly loading

--- directly-loading/src/index.js
+++ directly-loading/src/index.js
@@ -1,5 +1,4 @@
 function createPdfTtf() {
-    // Default export is a4 paper, portrait, using millimeters for units
     var doc = new jsPDF({
         filters: ["ASCIIHexEncode"],
         putOnlyUsedFonts: true,
@@ -13,6 +12,25 @@ function createPdfTtf() {
     doc.addFileToVFS("noto-sans-normal.ttf", notoSans);
     doc.addFont("noto-sans-normal.ttf", "NotoSansNormal", "normal")
     doc.setFont("NotoSansNormal", "normal")
+    doc.text('Hello world!', 10, 30)
+    console.log(doc);
+    doc.save('ttf.pdf')
+}
+
+function createPdfFromPtSansTtf() {
+    var doc = new jsPDF({
+        filters: ["ASCIIHexEncode"],
+        putOnlyUsedFonts: true,
+        floatPrecision: 2
+    })
+
+    doc.setFont("times", "italic")
+    doc.text('Hello world!', 10, 10)
+
+    const notoSans = doc.loadFile("./src/PTSans.ttf");
+    doc.addFileToVFS("PTSans.ttf", notoSans);
+    doc.addFont("PTSans.ttf", "PTSans", "normal")
+    doc.setFont("PTSans")
     doc.text('Hello world!', 10, 30)
     console.log(doc);
     doc.save('ttf.pdf')
