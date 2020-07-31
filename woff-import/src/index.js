63ec6ccf5581dbc242dba9ed4a8a69ae280cc0a0
nisk0817 <nikolai.skladnev@netcracker.com>
2020-07-31 07:24:15 +0400

    Remove woff format tests

--- woff-import/src/index.js
+++ woff-import/src/index.js
@@ -1,19 +1,3 @@
-function createPdfWoff() {
-    // Default export is a4 paper, portrait, using millimeters for units
-    var doc = new jsPDF()
-
-    doc.setFont("time", "italic")
-    doc.text('Hello world!', 10, 10)
-
-    const notoSans = doc.loadFile("./src/noto-sans-700.woff");
-    doc.addFileToVFS("noto-sans-700.woff", notoSans);
-    doc.addFont("noto-sans-700.woff", "NotoSans700", "bold")
-    doc.setFont("NotoSans700", "bold")
-    doc.text('Hello world!', 10, 30)
-    console.log(doc);
-    doc.save('woff.pdf')
-}
-
 function createPdfTtf() {
     // Default export is a4 paper, portrait, using millimeters for units
     var doc = new jsPDF({
@@ -36,26 +20,17 @@ function createPdfTtf() {
 
 function createPdfTtfBase64() {
     // Default export is a4 paper, portrait, using millimeters for units
-    var doc = new jsPDF()
+    var doc = new jsPDF({
+        filters: ["ASCIIHexEncode"],
+        putOnlyUsedFonts: true,
+        floatPrecision: 2
+    })
 
     doc.setFont("time", "italic")
     doc.text('Hello world!', 10, 10)
 
-    doc.setFont("NotoSansTtfBase64", "normal")
+    doc.setFont("NotoSansTtfBase64-normal", "normal")
     doc.text('Hello world!', 10, 30)
     console.log(doc);
     doc.save('ttfBase64.pdf')
-}
-
-function createPdfWoffBase64() {
-    // Default export is a4 paper, portrait, using millimeters for units
-    var doc = new jsPDF()
-
-    doc.setFont("time", "italic")
-    doc.text('Hello world!', 10, 10)
-
-    doc.setFont("NotoSansWoffBase64", "bold")
-    doc.text('Hello world!', 10, 30)
-    console.log(doc);
-    doc.save('woffBase64.pdf')
 }
